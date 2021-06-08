318cdcab3f717120fe8bfac59c0d125ce63169c0
Noureddine <noureddine.ballouki@gmail.com>
2021-06-08 03:37:00 +0100

    rv0.2

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+	image_fun = models.ImageField(upload_to='images/', null=True)
+	summary = models.CharField(max_length=200, null=True)
+		
